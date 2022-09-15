eec65bf9ff616b5b4771aa7cf0a2e39034772cb8
robertgotsik <robert.gotsik15@gmail.com>
2022-09-15 17:08:15 -0300

    First commit

---
+++ JWTToken.API/Models/UserConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTToken.API.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="poc",Password="poc_admin",Role="Admin"}
+            };
+    }
+}
